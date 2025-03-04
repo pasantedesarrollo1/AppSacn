da4536c87678e0371bbb4d58a3fa4b795ef2730c
Cristian Díaz <dannycriss12@gmail.com>
2025-03-04 11:59:41 -0500

    Icono funcional

--- android/app/src/main/java/com/tuempresa/appscan/MainActivity.java
+++ android/app/src/main/java/com/tuempresa/appscan/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.tuempresa.appscan;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,14 +2,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tuempresa.appscan',
-  appName: 'AppScan',
+  appName: 'App Visor Productos',
   webDir: 'dist',
-  bundledWebRuntime: false,
-  android: {
-    icon: "src/assets/icon",
-    iconForegroundSource: "src/assets/icon",
-    iconBackgroundColor: "#FFFFFF"
-  }
 };
 
 export default config;
